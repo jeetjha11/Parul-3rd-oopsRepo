d9f3f840c89777ae2e09180057d9dcc1ea1ccd44
Jeet Kumar Jha <jeetkrjha@gmail.com>
2024-06-28 11:36:33 +0530

    Files added

---
+++ ClassNotes_module/src/NewProgram.java
@@ -0,0 +1,25 @@
+import  java.util.Scanner;
+
+public class NewProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA");
+        Scanner scanner=new Scanner(System.in);
+
+
+
+
+        String name="Ram";
+        int a=44;
+        int b=55;
+        System.out.println("Addition is: "+ (a+b));
+
+        System.out.println("Name is: "+ name);
+
+        System.out.println("Enter the name");
+        String userName=scanner.next();
+        System.out.println("Hello, Good Morning "+ userName);
+
+
+
+    }
+}
